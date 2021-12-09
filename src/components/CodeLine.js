1e539e93d2e6f19f495a401c412c17c7c00b4b91
Ruben Kretek <rubenkretek@outlook.com>
2021-12-09 10:21:18 +0000

    Fix: removed character in codeline

--- src/components/CodeLine.js
+++ src/components/CodeLine.js
@@ -3,7 +3,7 @@ import React from "react";
 const CodeLine = ({ width, height, screenWidth, source }) => {
     return (
         <div className="codeline">
-            <span className="code-yellow">&lt;</span><span className="code-red">source</span> <span className="code-yellow">media</span><span className="code-red">=</span><span className="code-blue">"(min-width: {screenWidth}px)"</span> <span className="code-yellow">srcset</span><span className="code-red">=</span><span className="code-blue">"{source}"?width={width}</span><span className="code-yellow">&amp;amp;</span><span className="code-blue">height={height}</span><span className="code-yellow">&amp;amp;</span><span className="code-blue">crop=auto"</span><span className="code-yellow">&gt;</span>
+            <span className="code-yellow">&lt;</span><span className="code-red">source</span> <span className="code-yellow">media</span><span className="code-red">=</span><span className="code-blue">"(min-width: {screenWidth}px)"</span> <span className="code-yellow">srcset</span><span className="code-red">=</span><span className="code-blue">"{source}?width={width}</span><span className="code-yellow">&amp;amp;</span><span className="code-blue">height={height}</span><span className="code-yellow">&amp;amp;</span><span className="code-blue">crop=auto"</span><span className="code-yellow">&gt;</span>
         </div>
     )
 }
